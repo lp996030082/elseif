78082fc38ec2766c78dc2789e76282389c693022
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-04-14 07:39:27 -0300

    salvar

---
+++ exemple_if.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 30;
+
+
+if ($idade <= 17) {
+
+    echo ("sua idade é menor de 18 anos");
+}elseif ($idade >18){
+    echo("sua idade é menor de 18 anos");
+}else{
+    echo ("não corresponde a uma idade")
+}
+
+
+
+
+
+?>
